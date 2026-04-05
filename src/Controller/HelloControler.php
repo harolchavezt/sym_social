766a4c027addaf72172b1cc314f6bfd68b54867a
Harol <harol.chavez.t@gmail.com>
2026-04-05 00:29:35 -0400

    feat:routing basic first steps

--- src/Controller/HelloControler.php
+++ src/Controller/HelloControler.php
@@ -1,2 +1,28 @@
 <?php
 
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HelloControler extends AbstractController
+{
+    private array $messages = [
+        "Hello","Hi","Bye"
+    ];
+
+    #[Route('/{limit<\d+>?3}',name:'app_index')]
+    public function index(int $limit):Response
+    {
+        return new Response(
+            implode(',',array_slice($this -> messages,0,$limit))
+            );
+    }
+
+    #[Route('/messages/{id<\d+>}',name:'app_show_one', )]
+    public function showOne($id):Response
+    {
+        return new Response($this->messages[$id]);
+    }
+}
